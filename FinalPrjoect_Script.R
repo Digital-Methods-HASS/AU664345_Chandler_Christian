e47cce3128a2f71df090c2ec2fdd093592fa3bec
TheStarling <93544889+TheStarling@users.noreply.github.com>
2022-01-10 13:09:50 +0100

    Delete FinalPrjoect_Script.R

--- FinalPrjoect_Script.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-library(readr)
-library(ggplot2)
-library(dplyr)
-# Importing the dataset called list_of_emperors.csv
-list_of_emperors <- read_csv("~/List_of_emperors_1/list_of_emperors.csv.csv")
-View(list_of_emperors)
